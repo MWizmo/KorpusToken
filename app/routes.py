1d8159c9e760f8c238452db5687cf7e8f35cddb6
xKatinJn <49876341+xkatinjn@users.noreply.github.com>
2020-01-08 18:48:43 +0500

    Добавление командной анкеты Лимит анкет Пара мелких фиксов и фич

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 import datetime
 
 from app import app, db
-from app.models import User, Questions, QuestionnaireInfo, Questionnaire, Membership, Teams
+from app.models import User, Questions, QuestionnaireInfo, Questionnaire, Membership
 from flask import render_template, redirect, url_for, request
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, SignupForm, QuestionnairePersonal
+from app.forms import LoginForm, SignupForm, QuestionnairePersonal, QuestionnaireTeam
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -12,7 +12,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 @app.route('/home')
 @login_required
 def home():
-    user = {'username': str(User.query.all())}
+    user = {'name': User.query.filter_by(id=current_user.id).first().name}
     return render_template('homepage.html', title='KorpusToken', user=user)
 
 
@@ -44,19 +44,18 @@ def signup():
             email=form.email.data,
             login=form.login.data,
             tg_nickname=form.tg_nickname.data,
-            username=form.tg_nickname.data,
-            # team=str(form.team.data),
-            # role=str(form.role.data),
             courses=form.courses.data,
             birthday=form.birthday.data,
             education=form.education.data,
             work_exp=form.work_exp.data,
-            sex=form.sex.data)
+            sex=form.sex.data,
+            name=form.name.data,
+            surname=form.surname.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         user_team = Membership(user_id=User.query.filter_by(email=form.email.data).first().id,
-                               team_id=Teams.query.filter_by(name=form.team.data).first().id,
+                               team_id=form.team.data,
                                role_id=int(form.role.data))
         db.session.add(user_team)
         db.session.commit()
@@ -70,8 +69,28 @@ def questionnaire_self():
     if not current_user.is_authenticated:
         return redirect('login')
 
+    # Проверка на лимит голосования
+
+    try:
+        now = datetime.datetime.now()
+        td = str(now.year) + '-' + str(now.month)
+        db_date = Questionnaire.query.filter_by(user_id=current_user.id, type=1).first().date
+        last = str(db_date.year) + '-' + str(db_date.month)
+        print(last, td)
+        if last == td:
+            return render_template('questionnaire_error.html')
+    except:
+        with open('logs/log-{}.txt'.format(datetime.datetime.today()), 'w') as file:
+            file.write('Ошибка при проверке лимита голосования или пользователь проголосовал впервые')
+            file.write('user_id={}, name={}, surname={}, login={}'.format(
+                current_user.id,
+                User.query.filter_by(id=current_user.id).first().name,
+                User.query.filter_by(id=current_user.id).first().surname,
+                User.query.filter_by(id=current_user.id).first().login
+                       ))
+
     form = QuestionnairePersonal()
-    questions = Questions.query.filter_by(type=1)
+    questions = Questions.query.filter_by(type=1)       # type=1 - вопросы 1-го типа, т. е. личные
     if form.validate_on_submit():
         q = Questionnaire(user_id=current_user.id,
                           team_id=Membership.query.filter_by(user_id=current_user.id).first().team_id,
@@ -83,8 +102,6 @@ def questionnaire_self():
                  form.qst_selfcontrol.data, form.qst_strategy.data]
         i = 0
 
-        # ID СЕССИИ АНКЕТЫ (QUESTIONNAIRE_ID) ПЕРЕДАЕТСЯ НЕККОРЕКТНО
-
         for question in questions:
             answ = QuestionnaireInfo(question_id=question.id,
                                      questionnaire_id=Questionnaire.query.all()[-1].id,
@@ -94,9 +111,71 @@ def questionnaire_self():
             i += 1
         db.session.commit()
         return redirect(url_for('home'))
-    print(form.errors)
+
     return render_template('questionnaire_self.html', title='Личная анкета', form=form, q1=questions[0].text,
                            q2=questions[1].text, q3=questions[2].text, q4=questions[3].text)
+
+
+@app.route('/questionnaire_team', methods=['GET', 'POST'])
+def questionnaire_team():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    # Проверка на лимит голосования
+
+    try:
+        now = datetime.datetime.now()
+        td = str(now.year) + '-' + str(now.month)
+        db_date = Questionnaire.query.filter_by(user_id=current_user.id, type=2).first().date
+        last = str(db_date.year) + '-' + str(db_date.month)
+        print(last, td)
+        if last == td:
+            return render_template('questionnaire_error.html')
+    except:
+        with open('log-{}.txt'.format(datetime.datetime.today()), 'w') as file:
+            file.write('Ошибка при проверке лимита голосования')
+
+
+
+    teammates = []
+    lst_teammates_bd = Membership.query.filter_by(
+        team_id=Membership.query.filter_by(user_id=current_user.id).first().team_id)
+
+    for teammate in lst_teammates_bd:
+        if teammate.user_id == current_user.id:
+            continue
+        name = User.query.filter_by(id=teammate.user_id).first().name
+        surname = User.query.filter_by(id=teammate.user_id).first().surname
+        teammates.append({'id': teammate.user_id, 'name': '{} {}'.format(name, surname)})
+
+    form = QuestionnaireTeam()
+    questions = Questions.query.filter_by(type=2)
+
+    if form.validate_on_submit():
+        q = Questionnaire(user_id=current_user.id,
+                          team_id=Membership.query.filter_by(user_id=current_user.id).first().team_id,
+                          date=datetime.datetime.today(),
+                          type=2)
+
+        db.session.add(q)
+        db.session.commit()
+
+        answs = [form.qst_q1.data, form.qst_q2.data, form.qst_q3.data, form.qst_q4.data, form.qst_q5.data]
+        i = 0
+
+        for question in questions[:4]:
+            answ = QuestionnaireInfo(question_id=question.id,
+                                     questionnaire_id=Questionnaire.query.all()[-1].id,
+                                     question_num=i+1,
+                                     question_answ=answs[i])
+            db.session.add(answ)
+            i += 1
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('questionnaire_team.html', title='Командная анкета', teammates=teammates, form=form,
+                           q1=questions[0].text, q2=questions[1].text, q3=questions[2].text, q4=questions[3].text,
+                           q5=questions[4].text)
 
 
 @app.route('/logout')
