0af878d14ca925f967f28c62fa5492acc280ba89
xKatinJn <antosha.katin@yandex.ru>
2020-01-19 19:15:22 +0500

    Добавление оценки и осей в бд

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'mysql+pymysql://root:password@localhost/korpus_db'#.format(os.getenv('login'), os.getenv('pass'))
+        'mysql+pymysql://korpus_user:korpus_password@localhost/korpus_db'
+        #'mysql+pymysql://root:password@localhost/korpus_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ROLES = [('1', 'Front-end'), ('2', 'Проженный'), ('3', 'Рядовой'), ('4', 'Желторотик')]
