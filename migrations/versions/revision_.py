2ed77e37baff0474efd37ed724aa89d47b37afdc
Andrew Kuzin <AndrewKuzin@bitbucket.org>
2020-01-22 10:20:07 +0500

    Добавлены таблицы для оценки, подготовка страниц для процесса

---
+++ migrations/versions/revision_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: revision
+Revises: 22d1bd67af15
+Create Date: 2020-01-22 09:25:21.883569
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'revision'
+down_revision = '22d1bd67af15'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
