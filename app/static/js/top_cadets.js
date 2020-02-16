c2b8b590fb0ea0f96936586083f05f722d310160
MWizmo <masterwizmoo@yandex.ru>
2020-02-16 16:27:20 +0500

    Choosing top cadets

---
+++ app/static/js/top_cadets.js
@@ -0,0 +1,42 @@
+let app = angular.module("app", []);
+app.controller("ctrl", function ($scope, $http) {
+
+    $scope.counter = 0;
+    $scope.top_cadets=[];
+
+    $scope.choose = function(cadet_id){
+        let el = document.getElementById(cadet_id);
+        if (el.style.border == '2px solid blue'){
+                el.style.border = '0';
+                $scope.counter -= 1;
+                for(let i=0; i<$scope.top_cadets.length; i++)
+                    if ($scope.top_cadets[i]==cadet_id)
+                        $scope.top_cadets.splice(i, 1);
+            }
+        else
+            if ($scope.counter < 5){
+                el.style.border = '2px solid blue';
+                $scope.counter += 1;
+                $scope.top_cadets.push(cadet_id);
+            }
+    }
+
+    $scope.confirm = function(){
+        if ($scope.counter < 5){
+                alert('Выберите 5 курсантов');
+            }
+        else{
+            let user_data = {
+                'top_cadets':$scope.top_cadets
+            };
+	    $http({
+                method: 'POST',
+                url: '/confirm_top_cadets',
+                data: user_data,
+                async:false
+            }).then(function success (response) {
+                document.location.href = "home";
+            });
+        }
+    }
+    });
