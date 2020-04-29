a2552601e096bbec8f4b5a1ab1e6c42e7de2753b
Anton <49876341+xKatinJn@users.noreply.github.com>
2020-04-29 18:04:09 +0500

    Форматирование строки исправлено

--- app/api/users.py
+++ app/api/users.py
@@ -38,7 +38,8 @@ def register():
                 return response
 
             data['birthday'] = datetime.datetime.strptime(data['birthday'], '%Y-%m-%d')
-            token_word = f'{data["login"]}{data["email"]}{data["surname"]}{datetime.datetime.now().timestamp()}'
+            token_word = '{}{}{}{}'.format(data["login"], data["email"], data["surname"]
+                                           datetime.datetime.now().timestamp())                                  )
             token_word = hashlib.sha256(token_word.encode()).hexdigest()
             user = User(
                 data['email'],
